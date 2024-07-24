26055174d5b5c4b7a726bb86fab8285ecab7effb
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-23 23:17:40 -0700

    80. Remove Duplicates from Sorted Array II -> C++ Solution

---
+++ 01_Array/Q04-80. Remove Duplicates from Sorted Array II/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int idx = 0;
+        for(auto &it : nums){
+            if(idx == 0 || idx == 1 || nums[idx - 2] != it){
+                nums[idx] = it;
+                idx++;
+            }
+        }
+        return idx;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
