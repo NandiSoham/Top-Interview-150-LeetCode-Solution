e51151d8180765b258eac5a007122b2310ac3889
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-08-01 23:11:50 -0700

    274. H-Index -> C++ Solution

---
+++ 01_Array/Q11_274. H-Index/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(), citations.rend());
+        int n = citations.size();
+
+        for(int  i = 0; i < n; i++){
+            if(citations[i] >= i + 1) continue;
+            else return i;
+        }
+
+        return n;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
