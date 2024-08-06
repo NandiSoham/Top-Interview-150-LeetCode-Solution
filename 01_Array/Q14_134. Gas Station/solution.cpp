d6d9f6e69ea9d0cf04f7af6fb70cd5feb8e53a1b
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-08-05 23:34:41 -0700

    134. Gas Station -> C++ solution Greedy Approach

--- 01_Array/Q14_134. Gas Station/solution.cpp
+++ 01_Array/Q14_134. Gas Station/solution.cpp
@@ -30,7 +30,41 @@ public:
 };
 
 
-// time Complexity -> O(n^2)
+// time Complexity -> O(n^2) [it might give TLE]
+// Space Complexity -> O(1)
+
+// -----------------------------------------------------------------------------------------------
+
+// ---------------------------------Approach - 2 (Greedy Approach) -------------------------------
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+
+        int totalGasGain = accumulate(gas.begin(), gas.end(), 0);
+        int totalGasSpend = accumulate(cost.begin(), cost.end(), 0);
+
+        if(totalGasGain < totalGasSpend) return -1;
+
+        int currentGasBalance = 0;
+        int startStation = 0;
+
+        for(int i = 0; i < n; i++){
+            currentGasBalance = currentGasBalance + gas[i] - cost[i];
+            if(currentGasBalance < 0){
+                currentGasBalance = 0;
+                startStation = i + 1;
+            }
+        }
+
+        return startStation;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
 // Space Complexity -> O(1)
 
 // -----------------------------------------------------------------------------------------------
