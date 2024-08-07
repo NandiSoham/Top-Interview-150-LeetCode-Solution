e5e71f09d43feb4032032aebb786d21d9f9d3a59
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-08-06 23:48:31 -0700

    380. Insert Delete GetRandom O(1) -> Python Solution

---
+++ 01_Array/Q12_380. Insert Delete GetRandom O(1)/solution.py
@@ -0,0 +1,29 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.numList = []
+        self.numMap = {}
+
+    def insert(self, val: int) -> bool:
+        res = val not in self.numMap
+        if res:
+            self.numMap[val] = len(self.numList)
+            self.numList.append(val)
+        return res
+
+
+    def remove(self, val: int) -> bool:
+        res = val in self.numMap
+        if res:
+            idx = self.numMap[val]
+            lastValue = self.numList[-1]
+            self.numList[idx] = lastValue
+            self.numList.pop()
+            self.numMap[lastValue] = idx
+            del self.numMap[val]
+        return res
+
+    def getRandom(self) -> int:
+        return random.choice(self.numList)
+
+
